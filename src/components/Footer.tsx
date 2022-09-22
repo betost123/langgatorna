29d9d27020fe71d1c5618a50c7da0b01d59b3492
Betina <betina@weapp.se>
2022-09-22 18:14:38 +0200

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,73 @@
+import { Link } from "gatsby";
 import * as React from "react";
+import { Col, Grid, Row } from "react-flexbox-grid";
 import styled from "styled-components";
+import { ActionText, H4 } from "./Typography";
+import swishQrCode from "../images/swish-qr.png";
+import Spacing from "./Spacer";
 
 const Container = styled.div`
-  background-color: black;
-  width: 100vw;
-  padding: 2rem;
-  //position: absolute;
-  //bottom: 0;
-  left: 0;
   color: white;
-  min-height: 180px;
-  display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
+  padding: 1rem;
+`;
+
+const StyledMailto = styled.a`
+  text-decoration: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledImage = styled.img`
+  width: 130px;
 `;
 
 export const Footer: React.FC = () => {
-  return <Container>Drick mycket bärs</Container>;
+  return (
+    <Grid>
+      <Row end='xs'>
+        <Col md={3} xs={6}>
+          <H4 textAlign='left'>Kontakt</H4>
+          <StyledMailto href='mailto:betina@weapp.se'>
+            <ActionText textAlign='left'>Anders (CEO)</ActionText>
+          </StyledMailto>
+          <StyledMailto href='mailto:betina@weapp.se'>
+            <ActionText textAlign='left'>Rolf (CMO)</ActionText>
+          </StyledMailto>
+          <Spacing spacing={2} />
+          <ActionText textAlign='left'>Nordhemsgatan 18B</ActionText>
+          <ActionText textAlign='left'>413 06 Göteborg</ActionText>
+        </Col>
+
+        <Col md={3} xs={6}>
+          <H4 textAlign='left'>Sociala Medier</H4>
+          <StyledLink to='https://instagram.com/enlitenrolf'>
+            <ActionText textAlign='left'>Instagram</ActionText>
+          </StyledLink>
+          <StyledLink to='https://instagram.com/enlitenrolf'>
+            <ActionText textAlign='left'>TikTok</ActionText>
+          </StyledLink>
+          <StyledLink to='https://instagram.com/enlitenrolf'>
+            <ActionText textAlign='left'>Encrochat</ActionText>
+          </StyledLink>
+        </Col>
+
+        <Col md={3} xs={6}>
+          <H4 textAlign='left'>Genvägar</H4>
+          <StyledLink to='/news'>
+            <ActionText textAlign='left'>Jobba hos oss</ActionText>
+          </StyledLink>
+          <StyledLink to='/news'>
+            <ActionText textAlign='left'>Företagsinfo</ActionText>
+          </StyledLink>
+        </Col>
+
+        <Col md={3} xs={6}>
+          <H4 textAlign='right'>Stötta oss</H4>
+          <StyledImage src={swishQrCode} />
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
-import { Col, Row } from "react-flexbox-grid";
+import { Col, Grid, Row } from "react-flexbox-grid";
 import styled from "styled-components";
 import { MediaQuery } from "../utils/styling-helpers";
 import Spacing from "../components/Spacer";
@@ -31,67 +31,62 @@ const Title = styled.h1`
 const IndexPage = () => {
   return (
     <>
-      <Row center='xs'>
-        <Title>Välkommen till långgatorna</Title>
-      </Row>
-      <Spacing spacing={4} />
-      <InfoSlide
-        imageSrc={andersCubaLibre}
-        title='Långgatornas bästa cubra libre!'
-        text='Anders har gett sig ut för att hitta långgatornas bästa cuba libre. Vilken har helt enkelt bäst APK och smak?'
-        buttonText='Läs mer'
-        link='/news/anders-testar-cuba-libre'
-      />
-      <Spacing spacing={4} />
+      <Grid>
+        <Row center='xs'>
+          <Title>Välkommen till långgatorna</Title>
+        </Row>
+        <Spacing spacing={4} />
+        <InfoSlide
+          imageSrc={andersCubaLibre}
+          title='Långgatornas bästa cubra libre!'
+          text='Anders har gett sig ut för att hitta långgatornas bästa cuba libre. Vilken har helt enkelt bäst APK och smak?'
+          buttonText='Läs mer'
+          link='/news/anders-testar-cuba-libre'
+        />
+        <Spacing spacing={4} />
+      </Grid>
       <CallToActionBanner
         title='Vill du att din bar ska synas här?'
         actionText='Kontakta anders@mail.com'
         mailto='betina@weapp.se'
       />
       <Spacing spacing={4} />
+      <Grid>
+        <Col>
+          <H3>Se dagens erbjudanden</H3>
+        </Col>
+        <Row center='xs'>
+          {categoryCardData.map((category) => (
+            <CategoryCard
+              key={category.title}
+              imageSrc={category.imageSrc}
+              title={category.title}
+              navLink={category.navLink}
+            />
+          ))}
+        </Row>
+        <Spacing spacing={4} />
 
-      <Col>
-        <H3>Se dagens erbjudanden</H3>
-      </Col>
-      <Row center='xs'>
-        {categoryCardData.map((category) => (
-          <CategoryCard
-            key={category.title}
-            imageSrc={category.imageSrc}
-            title={category.title}
-            navLink={category.navLink}
-          />
-        ))}
-      </Row>
-      <Spacing spacing={4} />
-
-      <InfoSlide
-        imageSrc={rolfRestaurang}
-        title='Hundvänliga barer'
-        text='Rolf har sniffat upp de bästa barerna för doggos på långgatan och nu har han sammanställt en lista!'
-        buttonText='Ta del av roffes hundlista'
-        link='/news/hundvanliga-barer'
-        bgColor='#C5C0FF'
-        imageRight
-      />
-      <Spacing spacing={4} />
-      <InfoSlide
-        imageSrc={magasinetBild}
-        title='Heldag på långgatorna?'
-        text='Här är den ultimata guiden för dig som vill spendera 07.30 - 02:00 på långgatorna.'
-        buttonText='Läs mer'
-        link='/news/heldag-pa-langen'
-        bgColor='#A9D8C7'
-      />
-      <Spacing spacing={8} />
-
-      <Row>
-        {BarImages.map((image, index) => (
-          <Col md={6} key={index}>
-            <Image src={image.image} alt={image.alt} index={index} />
-          </Col>
-        ))}
-      </Row>
+        <InfoSlide
+          imageSrc={rolfRestaurang}
+          title='Hundvänliga barer'
+          text='Rolf har sniffat upp de bästa barerna för doggos på långgatan och nu har han sammanställt en lista!'
+          buttonText='Ta del av roffes hundlista'
+          link='/news/hundvanliga-barer'
+          bgColor='#C5C0FF'
+          imageRight
+        />
+        <Spacing spacing={4} />
+        <InfoSlide
+          imageSrc={magasinetBild}
+          title='Heldag på långgatorna?'
+          text='Här är den ultimata guiden för dig som vill spendera 07.30 - 02:00 på långgatorna.'
+          buttonText='Läs mer'
+          link='/news/heldag-pa-langen'
+          bgColor='#A9D8C7'
+        />
+        <Spacing spacing={8} />
+      </Grid>
     </>
   );
 };
